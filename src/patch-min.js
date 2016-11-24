20122ddf7d98c05762f599a2034ba80978d9e6ab
hville <hugov@runbox.com>
2016-11-24 11:52:59 +0800

    traverse in action instead of action in traverse

--- src/patch-min.js
+++ src/patch-min.js
@@ -15,91 +15,46 @@ module.exports = function (document, patch) {
 		if (!path) throw Error(errorMsg('path', path))
 		if (!op) throw Error(errorMsg('op', itm[0]))
 
-		result = op(result, itm, path)
+		result = op(result, path, itm[2])
 		if (result === undefined) return document //test failed, patch canceled
 	}
 	return result
 }
 
 var ops = {
-	t: function(res, itm, path) {
-		for (var i=0, val = res; i<path.length; ++i) {
-			if (!val) return
-			val = val[path[i]]
-		}
-		if (isEqual(val, itm[2])) return res
+	t: function(res, keys, ref) {
+		var val = get(res, keys)
+		if (val === undefined) return
+		if (isEqual(val, ref)) return res
 	},
-	d: function(res, itm, path) {
-		return pathAction(keyDel, res, path)
+	d: del,
+	a: add,
+	r: rep,
+	m: function(res, path, from) {
+		var val = get(res, from)
+		if (val === undefined) throw Error(errorMsg('from', from))
+		return add(del(res, from), path, val)
 	},
-	a: function(res, itm, path) {
-		return pathAction(keyAdd, res, path, itm[2])
-	},
-	r: function(res, itm, path) {
-		return pathAction(keyRep, res, path, itm[2])
-	},
-	m: function(res, itm, path) {
-		// validate that the from pointer has a vaild format
-		var from = itm[2]
-		if (!from) throw Error(errorMsg('from', itm[2]))
-
-		// validate that the from pointer is a valid reference
-		var val = res
-		for (var i=0; i<from.length; ++i) {
-			if (!val) throw Error(errorMsg('from', itm[2]))
-			val = val[from[i]]
-		}
-		res = pathAction(keyDel, res, from)
-		return pathAction(keyAdd, res, path, val)
-	},
-	c: function(res, itm, path) {
-		// validate that the from pointer has a vaild format
-		var from = itm[2]
-		if (!from) throw Error(errorMsg('from', itm[2]))
-
-		// validate that the from pointer is a valid reference
-		var val = res
-		for (var i=0; i<from.length; ++i) {
-			if (!val) throw Error(errorMsg('from', itm[2]))
-			val = val[from[i]]
-		}
-		return pathAction(keyAdd, res, path, val)
+	c: function(res, path, from) {
+		var val = get(res, from)
+		if (val === undefined) throw Error(errorMsg('from', from))
+		return add(res, path, val)
 	}
 }
+
 /**
  * Climbs and clones through the path before making change
- * @param {Function} action operation to be performed on leaf
- * @param {Object} result object to be cloned and changed
- * @param {Array} path patch item array of keys path
- * @param {?} [value] operation value
+ * @param {Object} obj object to be cloned and changed
+ * @param {Array} pth patch item array of keys path
  * @return {Object} modified result object
  */
-function pathAction(fcn, doc, path, value) {
-	if (!path.length) return value
-	if (!doc) throw Error(errorMsg('path', path.toString()))
-	if (path.length === 1) return fcn(doc, path[0], value)
-
-	var res = shallowClone(doc),
-			branch = res,
-			len = path.length - 2
-
-	var keyLeaf = path[len],
-			key = path[len + 1]
-
+function cloneLeaf(obj, pth) {
+	var len = pth.length -1 // stop before the last key
 	for (var i=0; i < len; ++i) {
-		branch = branch[path[i]] = shallowClone(branch[path[i]])
-		if (!branch) throw Error(errorMsg('path', path.toString()))
+		if (!obj[pth[i]]) throw Error(errorMsg('path', pth.toString()))
+		obj = obj[pth[i]] = shallowClone(obj[pth[i]])
 	}
-	// validate that the pointer is a valid reference up to the second last key
-	var leaf = branch[keyLeaf]
-	if (!leaf) throw Error(errorMsg('path', path.toString()))
-
-	// remaining key validations are done at the leaf operation level
-	var newLeaf = fcn(leaf, key, value)
-	if (newLeaf === leaf) return doc
-
-	branch[keyLeaf] = newLeaf
-	return res
+	return obj
 }
 /**
  * Format errors
@@ -112,46 +67,53 @@ function errorMsg(key, val) {
 }
 
 // BASIC KEY OPERATIONS
-
-function keyRep(obj, key, val) {
-	if (val === undefined) throw Error(errorMsg('value', val))
-	if (obj[key] === val) return obj
-	if (obj[key] === undefined) throw Error(errorMsg('path key', key))
-
-	var cln = shallowClone(obj)
-	cln[key] = val
-	return cln
-}
-
-function keyDel(obj, key) {
-	if (obj[key] === undefined) throw Error(errorMsg('path key', key))
-	var clone = shallowClone(obj)
-
-	if (Array.isArray(obj)) clone.splice(key, 1)
-	else delete clone[key]
-
-	return clone
-}
-
-function keyAdd(obj, key, val) {
-	if (val === undefined) throw Error(errorMsg('value', val))
-	var result = shallowClone(obj)
-
-	if (Array.isArray(obj)) {
-		// avoid comparison operators to prevent non digit number string conversions (eg "1e0")
-		if (key === '-' || key === '' + obj.length) {
-			result.push(val)
-			return result
-		}
-		if (obj[key] === undefined) throw Error(errorMsg('path key', key))
-
-		result.splice(key, 0, val)
-		return result
+function get(doc, pth) {
+	for (var i=0, val = doc; i<pth.length; ++i) {
+		if (!val) return
+		val = val[pth[i]]
 	}
+	return val
+}
+function rep(doc, pth, val) {
+	if (val === undefined) throw Error(errorMsg('value', val))
+	if (!pth.length) return val
 
-	if (obj[key] === val) return obj
-	result[key] = val
-	return result
+	var res = shallowClone(doc),
+			tgt = cloneLeaf(res, pth),
+			key = pth[pth.length-1]
+
+	if (tgt[key] === val) return doc //no change
+	if (tgt[key] === undefined) throw Error(errorMsg('path key', key))
+	tgt[key] = val
+	return res
+}
+function del(doc, pth) {
+	if (!pth.length) return
+	var res = shallowClone(doc),
+			tgt = cloneLeaf(res, pth),
+			key = pth[pth.length-1]
+
+	if (tgt[key] === undefined) throw Error(errorMsg('path key', key))
+	if (Array.isArray(tgt)) tgt.splice(key, 1)
+	else delete tgt[key]
+	return res
+}
+function add(doc, pth, val) {
+	if (val === undefined) throw Error(errorMsg('value', val))
+	if (!pth.length) return val
+	var res = shallowClone(doc),
+			tgt = cloneLeaf(res, pth),
+			key = pth[pth.length-1]
+	if (Array.isArray(tgt)) {
+		// avoid comparison operators to prevent non digit number string conversions (eg "1e0")
+		if (key === '-' || key === '' + tgt.length) tgt.push(val)
+		else if (tgt[key] === undefined) throw Error(errorMsg('path key', key))
+		else tgt.splice(key, 0, val)
+		return res
+	}
+	if (tgt[key] === val) return doc //no change
+	tgt[key] = val
+	return res
 }
 /**
  * @param {Object|Array} obj - object or array to be cloned
